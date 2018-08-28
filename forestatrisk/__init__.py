63fe8d4ceb87e94233a3f1ffb2f3b12cf9e9eb5f
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2018-08-28 12:06:19 +0200

    __future__ in init.py

--- forestatrisk/__init__.py
+++ forestatrisk/__init__.py
@@ -9,11 +9,12 @@
 # license         :GPLv3
 # ==============================================================================
 
+from __future__ import division, print_function  # Python 3 compatibility
 import os
 import matplotlib
-if os.environ.get('DISPLAY', '') == '':
-    print('no display found. Using non-interactive Agg backend')
-    matplotlib.use('Agg')
+if os.environ.get("DISPLAY", "") == "":
+    print("no display found. Using non-interactive Agg backend", flush=True)
+    matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import numpy as np
 # from data import country
