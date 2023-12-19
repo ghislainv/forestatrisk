9b8b0ed51b4f375194b575a49a147e23810eaac2
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2023-12-19 15:19:02 +1100

    Upgrade version number

--- forestatrisk/__init__.py
+++ forestatrisk/__init__.py
@@ -19,7 +19,7 @@ from __future__ import division, print_function  # Python 3 compatibility
 # https://peps.python.org/pep-0008/#module-level-dunder-names
 __author__ = "Ghislain Vieilledent"
 __email__ = "ghislain.vieilledent@cirad.fr"
-__version__ = "1.1.1"
+__version__ = "1.1.2"
 
 # Local imports
 # Data
