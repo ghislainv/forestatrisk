b462ddd238724747c8f2f76498e48d7e98bdee77
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2024-01-26 09:29:29 +1100

    Use text argument in subprocess.run to avoid .decode()

--- forestatrisk/data/compute/compute_osm.py
+++ forestatrisk/data/compute/compute_osm.py
@@ -22,9 +22,11 @@ def compute_osm(proj, extent, verbose=False):
 
     # Convert osm.pbf to o5m
     cmd = "osmconvert country.osm.pbf -o=country.o5m"
-    rtn = subprocess.run(cmd, capture_output=True, shell=True)
+    rtn = subprocess.run(cmd, shell=True,
+                         capture_output=True,
+                         text=True)
     if verbose:
-        print(rtn.stdout.decode())
+        print(rtn.stdout)
 
     # Filter data
     # Roads
@@ -32,25 +34,31 @@ def compute_osm(proj, extent, verbose=False):
            '--keep="highway=motorway '
            'or highway=trunk or highway=*ary" '
            '-o=roads.osm')
-    subprocess.run(cmd, capture_output=True, shell=True)
+    rtn = subprocess.run(cmd, shell=True,
+                         capture_output=True,
+                         text=True)
     if verbose:
-        print(rtn.stdout.decode())
+        print(rtn.stdout)
     # Towns
     cmd = ('osmfilter country.o5m '
            '--keep="place=city or '
            'place=town or place=village" '
            '-o=towns.osm')
-    subprocess.run(cmd, capture_output=True, shell=True)
+    rtn = subprocess.run(cmd, shell=True,
+                         capture_output=True,
+                         text=True)
     if verbose:
-        print(rtn.stdout.decode())
+        print(rtn.stdout)
     # Rivers
     cmd = ('osmfilter country.o5m '
            '--keep="waterway=river or '
            'waterway=canal" '
            '-o=rivers.osm')
-    subprocess.run(cmd, capture_output=True, shell=True)
+    rtn = subprocess.run(cmd, shell=True,
+                         capture_output=True,
+                         text=True)
     if verbose:
-        print(rtn.stdout.decode())
+        print(rtn.stdout)
 
     # Callback
     cback = gdal.TermProgress if verbose else 0
